700590d640f7f95c98b356ca965633223d634e9f
maechieee <agurojeremae20@gmail.com>
2026-05-02 20:18:52 +0800

    Added add user and load users feature

--- crud/views.py
+++ crud/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
-from .models import Genders
+from .models import Genders, Users
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 
@@ -77,3 +78,57 @@ def delete_gender(request, genderId):
       return render(request, 'gender/DeleteGender.html', data)
   except Exception as e:
     return HttpResponse(f'Error occured during delete gender: {e}')
+  
+def user_list(request):
+  try:
+    usersObj = Users.objects.select_related('gender') #SELECT * FROM tbl_users INNER JOIN tbl_genders ON tbl_users.gender_id = tbl_genders.gender_id;
+
+    data = {
+      'users': usersObj
+    }
+    
+    return render(request, 'user/UsersList.html', data)
+  except Exception as e:
+    return HttpResponse(f'Error occured during load users: {e}')
+  
+def add_user(request):
+  try:
+    if request.method == 'POST':
+      fullName = request.POST.get('full_name')
+      gender = request.POST.get('gender')
+      birthDate = request.POST.get('birth_date')
+      address = request.POST.get('address')
+      contactNumber = request.POST.get('contact_number') 
+      email = request.POST.get('email') 
+      username = request.POST.get('username')
+      password = request.POST.get('password')
+      confirmPassword = request.POST.get('confirm_password') 
+
+      if password != confirmPassword:
+        # if password and confirmPassword does not match, show error message
+        messages.error(request, 'Password and Confirm Password does not match!')
+        return redirect('/user/add/')
+
+      Users.objects.create(
+        full_name = fullName,
+        gender = Genders.objects.get(pk=gender), 
+        birth_date = birthDate,
+        address = address,
+        contact_number = contactNumber,
+        email = email,
+        username = username,
+        password = make_password(password) # hash the password before saving it
+      ).save() #INSERT INTO tbl_users(full_name, gender, birth_date, address, contact_number, email, username, password) VALUES (full_name, gender, birth_date, address, contact_number, email, username, password);
+    
+      messages.success(request, 'User added successfully!')
+      return redirect('/user/add/')
+    else:
+      genderObj = Genders.objects.all() #SELECT * FROM tbl_genders;
+
+      data = {
+        'genders': genderObj
+      }
+
+      return render(request, 'user/AddUser.html', data)
+  except Exception as e:
+    return HttpResponse(f'Error occured during add user: {e}')
